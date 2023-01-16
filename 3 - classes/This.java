b768c07115e538bf0fa5fabd3808dcbe18961f20
Andrew <andrewpratama22@gmail.com>
2023-01-16 14:51:52 +1100

    added more examples

--- 3 - classes/This.java
+++ 3 - classes/This.java
@@ -20,6 +20,14 @@ class MotoBike3 {
         this.top_speed = top_speed;
     }
 
+    // how "this" works
+    public void frobnicate() {
+        int cc = 1;
+        System.out.println(cc); // refers to the local variable a, prints 1
+        System.out.println(this.cc); // refers to the field a, prints the value of this.cc
+        System.out.println(this); // refers to this entire object
+    }
+
     // it's good practice to use "this" for clarity and less confusion
     int get_cc() {
         return this.cc;
